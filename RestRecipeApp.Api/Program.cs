372e5a34086643e0ffb653fb74cb8c59621f2e9f
Michelle <infi-gvb@MacBook-Pro-van-INFI-gvb.local>
2023-04-30 16:12:56 +0200

    expose swagger

--- RestRecipeApp.Api/Program.cs
+++ RestRecipeApp.Api/Program.cs
@@ -1,12 +1,12 @@
 using RestRecipeApp.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
-var  myCors = "_myCors";
+var myCors = "_myCors";
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: myCors,
-        policy  =>
+        policy =>
         {
             policy.WithOrigins("http://localhost:3000",
                 "http://localhost:3001").AllowAnyMethod().AllowAnyHeader();
@@ -17,11 +17,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers().AddNewtonsoftJson();
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseCors(myCors);
 
 app.MapGet("/", () => "Hello World!");
